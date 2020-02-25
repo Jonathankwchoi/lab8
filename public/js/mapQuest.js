cfe5b382ed086270b0136d3dd8f6d66801f7117e
Jonathankwchoi <jkc012@ucsd.edu>
2020-02-25 15:04:55 -0800

    thirdpartyAPI lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'pEmpp2GDBPYSTgS0umMDWQQcAJHgKCdO';
+
+	// 'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+		center: [32.8787884,-117.2373834],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 14,
+		zoomControl: false
+	});
+	L.marker([32.8787884,-117.2373834]).addTo(map);
 }
